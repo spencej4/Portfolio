1760c3a5707f8cc43f6c7ae04b444928a19af799
Spencer Jack <spencer.jack@alaskaair.com>
2018-12-09 10:56:23 -0800

    updates mobile nav menu transitions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,42 +9,30 @@ $('.hamburger_click_me').click(function() {
 });
 
 function toggleMobileMenu() {
-    console.log('toggleMobileMenu ran');
-    console.log(`MobileMenuRendered: ${mobileMenuRendered}`);
-    // if ($('#main_navbar').hasClass('hidden')) {
     if (mobileMenuRendered === false){
         mobileMenuRendered = true;
-        $("#main_navbar").animate({
-            width: "toggle"
-        });
-        $("#side_nav_menu").fadeIn( "fast");
-        $("#side_nav_menu").removeClass('slideLeft')
-        $("#full_page_opacity_screen").fadeIn( "fast");
-        
-        // $('#main_navbar').removeClass('hidden');
-        // $('#main_navbar').addClass('show');
-        // $('#full_page_opacity_screen').removeClass('hidden');
-        // $('#full_page_opacity_screen').addClass('show');
+        // $("#main_navbar").animate({
+        //     width: "toggle"
+        // });
+        $(".main_navbar").toggleClass('animate');
+
+        // original
+        // $("#main_navbar").animate({left: '0px'});
+        $(".side_nav_menu").fadeIn( "fast");
+        return
     } 
-    // else if ($('#main_navbar').hasClass('show')){
     else if (mobileMenuRendered === true){
         mobileMenuRendered = false;
-        $("#main_navbar").animate({
-            width: "toggle"
-        });
-        $("#side_nav_menu").fadeIn( "fast");
-        $("#side_nav_menu").removeClass('slideLeft')
-        $("#full_page_opacity_screen").css('display', 'none');
+        // $("#main_navbar").animate({width: "toggle"});
+        $(".main_navbar").toggleClass('animate');
 
-    //     mobileMenuRendered = false;
-    //     //hide nav menu
-    //     $('#main_navbar').removeClass('show');
-    //     // $('#main_navbar').addClass('hidden');
-    //     //add opacity screen
-    //     $('#full_page_opacity_screen').removeClass('show');
-    //     $('#full_page_opacity_screen').addClass('hidden');
+        // original
+        // $("#main_navbar").animate({left: '-290px'});
+        $("#side_nav_menu").fadeOut( "fast");
+        return
+        // $("#side_nav_menu").removeClass('slideLeft')
+        // $("#full_page_opacity_screen").css('display', 'none');
     }
-    console.log(`At end of function MobileMenuRendered: ${mobileMenuRendered}`);
 }
 
 // changes page content based on nav click
